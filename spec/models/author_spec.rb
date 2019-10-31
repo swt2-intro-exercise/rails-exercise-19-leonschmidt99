c7a9c7464c9e2247dabb768547436efa3cae857d
Leon Schmidt <leon.hand09@gmail.com>
2019-10-31 15:30:04 +0100

    Actually added the author model spec

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,17 @@
+describe "Author model", type: :model do
+
+  before :each do
+    @author = Author.new('Alan', 'Turing', 'http://wikipedia.org/wiki/Alan_Turing')
+  end
+
+  it "should have attributes for first name, last name and homepage" do
+    expect(@author.first_name).to eq('Alan')
+    expect(@author.last_name).to eq('Turing')
+    expect(@author.homepage).to eq('http://wikipedia.org/wiki/Alan_Turing')
+  end
+
+  it "should return the full name when calling name()" do
+    expect(@author.name()).to eq('Alan Turing')
+  end
+
+end

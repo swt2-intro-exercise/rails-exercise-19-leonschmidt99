f76a2952cce7bf293dd5ac61d98416534e92f2a4
Leon Schmidt <leon.hand09@gmail.com>
2019-10-31 15:53:26 +0100

    Added test for creating Authors via the new author page

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -2,21 +2,28 @@ require 'rails_helper'
 
 describe "New author page", type: :feature do
   
-  it "should render withour error" do
+  before :each do
     visit new_author_path
   end
 
-  it "should contain text input fields for first_name, last_name and homepage" do
-    visit new_author_path
+  it "should render withour error" do
+  end
 
+  it "should contain text input fields for first_name, last_name and homepage" do
     expect(page).to have_field('author[first_name]')
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
 
   it "should contain a button for submitting the form" do
-    visit new_author_path
-
     expect(page).to have_css('input[type="submit"]')
+  end
+
+  it "should be able to create a new Author entry in the database" do
+    page.fill_in 'author[first_name]', with: 'Alan'
+    page.fill_in 'author[last_name]', with: 'Turing'
+    page.fill_in 'author[homepage]', with: 'http://wikipedia.org/wiki/Alan_Turing'
+
+    find('input[type="submit"]').click
   end
 end

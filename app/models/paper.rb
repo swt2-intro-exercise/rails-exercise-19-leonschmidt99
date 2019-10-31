dffb8c851d9f6964a712ef9052ae9d3ccc6e9f1b
Leon Schmidt <leon.hand09@gmail.com>
2019-10-31 22:14:39 +0100

    Added paper index filtering by year, may add real test later

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,4 +2,8 @@ class Paper < ApplicationRecord
   has_and_belongs_to_many :authors
   validates :title, :venue, presence: true
   validates :year, numericality: true
+  
+  def self.published_in(query_year)
+    where("year == ?", query_year)
+  end
 end
--- spec/views/papers/index.html.erb_spec.rb
+++ spec/views/papers/index.html.erb_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "papers/index", type: :view do
   before(:each) do
+=begin
     assign(:papers, [
       Paper.create!(
         :title => "Title",
@@ -14,12 +15,19 @@ RSpec.describe "papers/index", type: :view do
         :year => 2
       )
     ])
+=end
+  @papers = [create(:paper), create(:paper)]
   end
 
   it "renders a list of papers" do
     render
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
-    assert_select "tr>td", :text => "Venue".to_s, :count => 2
-    assert_select "tr>td", :text => 2.to_s, :count => 2
+    assert_select "tr>td", :text => "COMPUTING MACHINERY AND INTELLIGENCE".to_s, :count => 2
+    assert_select "tr>td", :text => "Mind 49: 433-460".to_s, :count => 2
+    assert_select "tr>td", :text => 1950.to_s, :count => 2
   end
+
+  #it "can be filtered according to year" do
+  #  get :index, {:year => 1950}
+  #  expect(rendered).to_not have_text("COMPUTING MACHINERY AND INTELLIGENCE")
+  #end
 end

bc931b11d7759199ee902fc333be41d47f188076
Leon Schmidt <leon.hand09@gmail.com>
2019-10-31 20:58:48 +0100

    Added test for paper detail page showing authors

--- spec/views/papers/show.html.erb_spec.rb
+++ spec/views/papers/show.html.erb_spec.rb
@@ -2,17 +2,24 @@ require 'rails_helper'
 
 RSpec.describe "papers/show", type: :view do
   before(:each) do
-    @paper = assign(:paper, Paper.create!(
-      :title => "Title",
-      :venue => "Venue",
-      :year => 2
-    ))
+  #  @paper = assign(:paper, Paper.create!(
+  #    :title => "Title",
+  #    :venue => "Venue",
+  #    :year => 2
+  #  ))
+    @paper = create(:paper)
   end
 
   it "renders attributes in <p>" do
     render
     expect(rendered).to match(/Title/)
     expect(rendered).to match(/Venue/)
-    expect(rendered).to match(/2/)
+    expect(rendered).to match(/Year/)
+    expect(rendered).to match(/Authors/)
+  end
+
+  it "contains a link to its author" do
+    render
+    expect(rendered).to have_link 'Alan Turing', href: author_path(@paper.authors.first)
   end
 end

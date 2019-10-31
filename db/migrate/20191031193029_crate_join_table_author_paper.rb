439563f55633146dca181bbbf025bcbb2064eb98
Leon Schmidt <leon.hand09@gmail.com>
2019-10-31 20:33:23 +0100

    Included db migration file

---
+++ db/migrate/20191031193029_crate_join_table_author_paper.rb
@@ -0,0 +1,8 @@
+class CrateJoinTableAuthorPaper < ActiveRecord::Migration[5.2]
+  def change
+    create_join_table :authors, :papers do |t|
+      # t.index [:author_id, :paper_id]
+      # t.index [:paper_id, :author_id]
+    end
+  end
+end

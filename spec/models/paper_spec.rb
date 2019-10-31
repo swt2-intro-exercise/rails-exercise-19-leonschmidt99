4643f6604da927617122674c14d1f77fb26ddb75
Leon Schmidt <leon.hand09@gmail.com>
2019-10-31 20:22:18 +0100

    Added tests for paper validity checks

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @paper = build(:paper)
+  end
+
+  it "does not validate without title" do
+    @paper.title = nil
+    expect(@paper).to_not be_valid
+  end
+
+  it "does not validate without venue" do
+    @paper.venue = nil
+    expect(@paper).to_not be_valid
+  end
+
+  it "does not validate without a numeral for a year" do
+    @paper.year = nil
+    expect(@paper).to_not be_valid
+  end
 end
